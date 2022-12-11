e2bf57ad24d968df98906cbda49e9e1a47a40448
WambuiNicole <wambuinicole99@gmail.com>
2022-12-11 10:48:00 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * *main- prints to screen
+ *    *
+ *       *Return: zero
+ */
+int main()
+{
+	char quote[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+
+	putchar(quote);
+	return (1);
+}
