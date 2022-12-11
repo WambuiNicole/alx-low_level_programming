6fa7d73d4f15b320f75970208c9aabb2c52c3b60
WambuiNicole <wambuinicole99@gmail.com>
2022-12-11 05:54:13 -0800

    puts-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+/*
+ * main-entry point
+ * use puts instead of printf with a new line
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
