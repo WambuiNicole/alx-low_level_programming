5fd0eadbef13974c7162607f1feff731146bb9fc
WambuiNicole <wambuinicole99@gmail.com>
2022-12-11 06:06:15 -0800

    puts-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /*
- * main-entry point
- * use puts instead of printf with a new line
+ * main-print to screen
+ * returns zero
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
