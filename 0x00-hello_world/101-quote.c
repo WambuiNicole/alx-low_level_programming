7e8c954a6941bdce16786ce56be4f737f6cddf55
WambuiNicole <wambuinicole99@gmail.com>
2022-12-11 10:50:43 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main()
 {
-	char quote[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	char quote[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	putchar(quote);
 	return (1);
